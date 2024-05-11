b7d00516f12f3165bcf11b7e2e771389c43b6762
CoolShivas <shivas2710cool00@gmail.com>
2024-05-11 21:17:10 +0530

    Passing Data via Props

--- swiggy_app/src/components/main/Body.jsx
+++ swiggy_app/src/components/main/Body.jsx
@@ -4,24 +4,25 @@ import RestroCard from "./RestroCard";
 const Body = () => {
   return (
     <>
-    <div className="body">
+      <div className="body">
         <div className="search_bar">
-            Search Bar
-            <div className={styles.restro_container}>
-                <RestroCard></RestroCard>
-                <RestroCard></RestroCard>
-                <RestroCard></RestroCard>
-                <RestroCard></RestroCard>
-                <RestroCard></RestroCard>
-                <RestroCard></RestroCard>
-                <RestroCard></RestroCard>
-                <RestroCard></RestroCard>
-                <RestroCard></RestroCard>
-                <RestroCard></RestroCard>
-                <RestroCard></RestroCard>
-            </div>
+          Search Bar
+          <div className={styles.restro_container}>
+
+            <RestroCard resName={"Meghana Foods"}
+              resCuisine={"Cheese Burger, North Indian, Asian"}
+              resStars={4.8}
+              resTiming={45}
+            ></RestroCard>
+            <RestroCard resName={"KFC"}
+              resCuisine={"Butter Chicken, World Famous"}
+              resStars={5}
+              resTiming={25}
+            ></RestroCard>
+
+          </div>
         </div>
-    </div>
+      </div>
     </>
   )
 }
--- swiggy_app/src/components/main/RestroCard.jsx
+++ swiggy_app/src/components/main/RestroCard.jsx
@@ -1,20 +1,20 @@
 import styles from "./RestroCard.module.css";
 
-const RestroCard = () => {
+const RestroCard = (props) => {
   return (
     <>
       <div className={styles.restro_card}>
         <div className={styles.restro_card__images}>
-          
-            <img src="../../../public/pexels-ash-122861-376464.jpg" alt="image not found" />
-          
+
+          <img src="../../../public/pexels-ash-122861-376464.jpg" alt="image not found" />
+
         </div>
 
         <div className={styles.restro_card__details}>
-          <h3> Meghana Foods </h3>
-          <h4> Cheese Burger, North Indian, Asian </h4>
-          <h4> 4.4 stars </h4>
-          <h4> 38 minutes </h4>
+          <h3> {props.resName} </h3>
+          <h4> {props.resCuisine} </h4>
+          <h4> {props.resStars} stars </h4>
+          <h4> {props.resTiming} minutes </h4>
         </div>
       </div>
     </>
